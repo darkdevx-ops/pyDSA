3b5fd82389a4585c02d8aa4642947746997f99ea
Dardev <sbadal745@gmail.com>
2026-02-25 12:19:58 +0530

    implentation of doubly linked list and fix some bugs in previos singly linked list code and update README

--- README.md
+++ README.md
@@ -14,3 +14,18 @@
 -- 4. deleteNode(value) :- this function is used to delete a node based on the value of the node from the linked list.
 --- eg. deleteNode(20) >>> 10 15
 -- 5. printNode() :- this function is used to print the nodes of linked list.
+
+# 1. Doubly Linked List :
+
+-- The 2nd program or code is for implementation of doubly linked list using Python.
+-- I have created two different class in this, 1 for creating a Node and another 1 for managing node using some function.
+-- Functions are
+-- 1. insertionAtBeg(value) :- this function is used to add node at the begining of linked list.
+--- eg. insertionAtBeg(10) >>> 10
+-- 2. insertionAtEnd(value) :- this function is used to add node at the end of linked list.
+--- eg. insertionAtEnd(20) >>> 10 20
+-- 3. insertionAtMid(value, location) :- this function is used to add node at the middle of linked list based on the given location(value of a node already present in linked list) else it add the node at the end of linked list by default.
+--- eg. insertionAtMid(15,30) >>> 10 20 15
+-- 4. deleteNode(value) :- this function is used to delete a node based on the value of the node from the linked list.
+--- eg. deleteNode(20) >>> 10 15
+-- 5. printNode() :- this function is used to print the nodes of linked list.
--- doublyll.py
+++ doublyll.py
@@ -1,20 +1,22 @@
 class Node :
-  def __init__(self, data, next = None):
+  def __init__(self, data, next= None, prev= None):
     self.data = data
     self.next = next
+    self.prev = prev
 
 
-class SinglyLinkedList :
-  def  __init__(self, head= None):
+class DoubleLinkedList :
+  def __init__(self, head = None):
     self.head = head
 
   def insertionAtBeg(self, value):
     temp = Node(value)
-    if(self.head == None):
+    t1 = self.head
+    if(t1 == None):
       self.head = temp
-        
-    else :
-      temp.next = self.head
+    else:
+      t1.prev = temp
+      temp.next = t1
       self.head = temp
 
   def insertionAtEnd(self, value):
@@ -24,6 +26,7 @@ class SinglyLinkedList :
       while(t1.next != None):
         t1= t1.next
       t1.next = temp
+      temp.prev = t1
     else :
       self.head = temp
 
@@ -34,76 +37,77 @@ class SinglyLinkedList :
     while(t1.next != None):
       if(t1.data == x):
         temp.next = t1.next
+        t1.next.prev = temp
         t1.next = temp
+        temp.prev = t1
         return
       t1 = t1.next
     t1.next = temp
-    
+    temp.prev = t1
+
   def deleteNode(self, value):
     t1 = self.head
-    t0 = self.head
     if(t1 == None):
       # print("Linked list is empty.")
       return
     else:
-
-      if(t1.data == value):
+      if(t1.next == None or t1.data == value):
         t1 = t1.next
         self.head = t1
         return
     
     while(t1.next != None):
       if(t1.data == value):
-        t0.next = t1.next
+        t1.prev.next = t1.next
+        t1.next.prev = t1.prev
         return
       else :
-        t0 = t1
         t1 = t1.next
     
     if(t1.data == value):
-      t0.next = None
-      t1 = t0
+      t1.prev.next = None
     else :
       print("No node exist for delete.")
 
   def printNode(self):
-    temp = self.head
-    if(temp == None):
+    t1 = self.head
+    if(t1 == None):
       print("Linked list is empty.")
       return
     else:
-      while(temp.next != None):
-        print(temp.data, end= " --> ")
-        temp = temp.next
-      print(temp.data)
-
-    
+      while(t1.next != None):
+        print(t1.data, end= " <--> ")
+        t1 = t1.next
+      print(t1.data)
 
 
-
-obj1 = SinglyLinkedList()
+obj1 = DoubleLinkedList()
 obj1.insertionAtBeg(10)
+obj1.insertionAtBeg(20)
 obj1.insertionAtBeg(30)
-obj1.insertionAtBeg(50)
 obj1.insertionAtEnd(5)
 obj1.insertionAtEnd(15)
-obj1.insertionAtMid(35,10)
-obj1.insertionAtMid(45,15)
-obj1.insertionAtMid(25,50)
+obj1.insertionAtMid(25, 10)
+obj1.insertionAtMid(35, 30)
+obj1.insertionAtMid(45, 20)
 obj1.deleteNode(10)
-obj1.deleteNode(50)
 obj1.deleteNode(45)
-obj1.deleteNode(25)
+obj1.deleteNode(20)
+obj1.deleteNode(30)
+obj1.deleteNode(35)
 obj1.deleteNode(5)
 obj1.deleteNode(15)
-obj1.deleteNode(35)
-obj1.deleteNode(30)
+obj1.deleteNode(25)
 obj1.deleteNode(42)
 
 
 
+
+
+
+
+
+
+
 obj1.printNode()
-
-
-
-
+    
